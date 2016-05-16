fd57cde34ef547e673d4fde4933b4a95939c63b2
Huai-Ming <1604723751@qq.com>
2016-05-16 13:35:45 +0800

    Add Redis samples to the github

---
+++ RedisSamples/RedisSamples/Program.cs
@@ -0,0 +1,33 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedisSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "localhost";
+            string elementKey = "testKeyRedis";
+
+            using (RedisClient redisClient = new RedisClient(host, 6379, "foobared"))
+            {
+                if (redisClient.Get<string>(elementKey) == null)
+                {
+                    // adding delay to see the difference
+                    Thread.Sleep(5000);
+                    // save value in cache
+                    redisClient.Set(elementKey, "some cached value");
+                }
+                // get value from the cache by key
+                Console.WriteLine("Item value is: " + redisClient.Get<string>(elementKey));
+            }
+            Console.Read();
+        }
+    }
+}
